44305eb2901db5ef6386678b9fa0797fd4a96088
PresswerCat <presswercats@gmail.com>
2025-07-11 22:24:41 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     vueReactivityTransform(),
     vueDefineModels()
   ],
-  base: '/CatsProject/',
+  base: '/CATSProject/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
